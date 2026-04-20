c2a21d63c9c6e03362153bc865cc434d76780c2a
aashishgurunggrk-maker <aashishgurung.grk@gmail.com>
2026-04-20 11:38:18 +0545

    divisibleby 5 & 7

---
+++ divisibleby57.py
@@ -0,0 +1,8 @@
+num = int(input(" enter the number "))
+if num == 0:
+    print (" the number is 0 ")
+elif num % 5==0:
+    print("the number is divisible by 5")
+if num % 7==0:
+    print("the number is divisible by 7")
+
